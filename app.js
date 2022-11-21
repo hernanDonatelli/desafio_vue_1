fa0fde3197a64d278f705f84c0dda962c41d2595
Hernan Donatelli <hernandonatelli@gmail.com>
2022-11-21 18:34:11 -0300

    subida de desafio clase 1 de VueJs

---
+++ app.js
@@ -0,0 +1,15 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    titulo: "Desafio 1 - Contador",
+    contador: 0
+  },
+  methods: {
+    sumar(){
+      this.contador ++;
+    },
+    restar(){
+      this.contador --;
+    }
+  },
+})
